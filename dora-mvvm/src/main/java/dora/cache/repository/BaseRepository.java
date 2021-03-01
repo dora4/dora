4ba174381513036c025f794b1962924ac94faa3d
alm <dora924666990@gmail.com>
2021-03-01 16:38:41 +0800

    【DORA-25】(进一步优化三级缓存框架)https://github.com/users/dora4/projects/1#column-12502530

--- dora-mvvm/src/main/java/dora/cache/repository/BaseRepository.java
+++ dora-mvvm/src/main/java/dora/cache/repository/BaseRepository.java
@@ -15,17 +15,32 @@ import dora.util.NetworkUtils;
 
 /**
  * 数据仓库，扩展它来支持数据的三级缓存，即从云端服务器的数据库、手机本地数据库和手机内存中读取需要的数据，以支持用户
- * 手机在断网情况下也能显示以前的数据。
+ * 手机在断网情况下也能显示以前的数据。一个{@link BaseRepository}要么用于非集合数据，要么用于集合数据。如果要用于
+ * 非集合数据，请配置{@link Repository}注解将{@link #mListData}的值设置为false。
  */
 abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IListDataFetcher<T> {
 
+    /**
+     * 缓存策略。
+     */
     protected int mCacheStrategy = DataSource.CacheStrategy.NO_CACHE;
 
+    protected static final String TAG = "BaseRepository";
+
+    /**
+     * 非集合数据获取接口。
+     */
     protected IDataFetcher<T> mDataFetcher;
 
+    /**
+     * 集合数据获取接口。
+     */
     protected IListDataFetcher<T> mListDataFetcher;
 
-    protected boolean mListData;
+    /**
+     * true代表用于集合数据，false用于非集合数据。
+     */
+    protected boolean mListData = true;
 
     {
         Repository repository = getClass().getAnnotation(Repository.class);
@@ -40,6 +55,18 @@ abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IL
         }
     }
 
+    public boolean isListData() {
+        return mListData;
+    }
+
+    public void setDataFetcher(IDataFetcher<T> fetcher) {
+        this.mDataFetcher = fetcher;
+    }
+
+    public void setListDataFetcher(IListDataFetcher<T> fetcher) {
+        this.mListDataFetcher = fetcher;
+    }
+
     /**
      * 是否在网络加载数据失败的时候清空数据库的缓存。
      *
@@ -49,20 +76,74 @@ abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IL
         return false;
     }
 
-    protected IDataFetcher<T> installDataFetcher() {
-        return mDataFetcher;
+    /**
+     * 安装默认的非集合数据抓取。
+     *
+     * @return
+     */
+    protected abstract IDataFetcher<T> installDataFetcher();
+
+    /**
+     * 安装默认的集合数据抓取。
+     *
+     * @return
+     */
+    protected abstract IListDataFetcher<T> installListDataFetcher();
+
+    @Override
+    public DoraCallback<T> callback() {
+        return new DoraCallback<T>() {
+            @Override
+            public void onSuccess(T data) {
+                Logger.dtag(TAG, data.toString());
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Logger.dtag(TAG, code+":"+msg);
+            }
+        };
     }
 
-    protected IListDataFetcher<T> installListDataFetcher() {
-        return mListDataFetcher;
+    @Override
+    public DoraListCallback<T> listCallback() {
+        return new DoraListCallback<T>() {
+            @Override
+            public void onSuccess(List<T> data) {
+                for (T t : data) {
+                    Logger.dtag(TAG, t.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Logger.dtag(TAG, code+":"+msg);
+            }
+        };
     }
 
+    /**
+     * 非集合数据的API接口调用。
+     *
+     * @param callback
+     */
     protected void onLoadFromNetwork(DoraCallback<T> callback) {
     }
 
+    /**
+     * 集合数据的API接口调用。
+     *
+     * @param callback
+     */
     protected void onLoadFromNetwork(DoraListCallback<T> callback) {
     }
 
+    /**
+     * 从三级缓存仓库选择数据。
+     *
+     * @param ds 数据的来源
+     * @return 数据是否获取成功
+     */
     protected boolean selectData(@NonNull DataSource ds) {
         if (mCacheStrategy == DataSource.CacheStrategy.NO_CACHE) {
             if (isNetworkAvailable()) {
@@ -103,6 +184,9 @@ abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IL
         return false;
     }
 
+    /**
+     * 数据的来源。
+     */
     public interface DataSource {
 
         enum CacheType {
@@ -128,7 +212,17 @@ abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IL
             int MEMORY_CACHE = 2;
         }
 
+        /**
+         * 从缓存中加载数据。
+         *
+         * @param type
+         * @return
+         */
         boolean loadFromCache(CacheType type);
+
+        /**
+         * 从服务器/网络加载数据。
+         */
         void loadFromNetwork();
     }
 
@@ -148,6 +242,11 @@ abstract class BaseRepository<T extends OrmTable> implements IDataFetcher<T>, IL
         return mListDataFetcher.getListData();
     }
 
+    /**
+     * 检测网络是否可用。
+     *
+     * @return
+     */
     protected boolean isNetworkAvailable() {
         return NetworkUtils.checkNetwork();
     }
