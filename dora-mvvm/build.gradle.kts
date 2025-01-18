4cd53bd03ab98c87605d113dd554dcc4e62323cd
dora <dora924666990@gmail.com>
2025-01-18 20:27:52 +0800

    Added a tool to show and hide the system bar.(https://github.com/dora4/dora/issues/17)

--- dora-mvvm/build.gradle.kts
+++ dora-mvvm/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "dora.mvvm"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         minSdk = 21
     }
@@ -28,15 +28,15 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-    implementation("androidx.core:core:1.13.0")
+    implementation("androidx.core:core:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.android.support:design:28.0.0")
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
-    api("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.6")
-    api("androidx.activity:activity-ktx:1.9.2")
-    api("androidx.fragment:fragment-ktx:1.8.4")
+    api("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    api("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.8.7")
+    api("androidx.activity:activity-ktx:1.10.0")
+    api("androidx.fragment:fragment-ktx:1.8.5")
 
     api("io.reactivex.rxjava2:rxjava:2.2.19")
 }
@@ -48,7 +48,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora"
-                version = "1.2.41"
+                version = "1.2.42"
             }
         }
     }
