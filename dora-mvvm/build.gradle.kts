23dfd96d5481d1431915f3494cc06da0dccdcf55
dora <dora924666990@gmail.com>
2025-01-18 20:50:36 +0800

    Added a tool to show and hide the system bar.(https://github.com/dora4/dora/issues/17)

--- dora-mvvm/build.gradle.kts
+++ dora-mvvm/build.gradle.kts
@@ -48,7 +48,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora"
-                version = "1.2.44"
+                version = "1.2.45"
             }
         }
     }
