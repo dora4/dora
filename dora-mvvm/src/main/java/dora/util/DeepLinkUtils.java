d2348ee87f87eb0c2ed4464c6d42b42977be9ad5
dora <dora924666990@gmail.com>
2025-08-30 13:13:15 +0800

    Add deep links.

--- dora-mvvm/src/main/java/dora/util/DeepLinkUtils.java
+++ dora-mvvm/src/main/java/dora/util/DeepLinkUtils.java
@@ -6,6 +6,10 @@ import android.net.Uri;
 
 public class DeepLinkUtils {
 
+    /**
+     * Open a Discord user page.
+     * 简体中文：打开 Discord 用户页面。
+     */
     public static void openDiscordUser(Context context, String userId) {
         Uri discordUri = Uri.parse("discord://users/" + userId);
         Uri webUri = Uri.parse("https://discord.com/users/" + userId);
@@ -19,6 +23,10 @@ public class DeepLinkUtils {
         }
     }
 
+    /**
+     * Open a Discord server invitation page.
+     * 简体中文：打开 Discord 群邀请页面。
+     */
     public static void openDiscordGroup(Context context, String inviteCode) {
         Uri discordUri = Uri.parse("discord://invite/" + inviteCode);
         Uri webUri = Uri.parse("https://discord.com/invite/" + inviteCode);
