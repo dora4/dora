73b2f4f246aa7e05c70f3085364cd3f7742492d4
dora <dora924666990@gmail.com>
2025-08-30 13:04:34 +0800

    Add deep links.

--- dora-mvvm/src/main/java/dora/util/DeepLinkUtils.kt
+++ dora-mvvm/src/main/java/dora/util/DeepLinkUtils.kt
@@ -25,15 +25,12 @@ object DeepLinkUtils {
     fun openDiscordGroup(context: Context, inviteCode: String) {
         val discordUri = Uri.parse("discord://invite/$inviteCode")
         val webUri = Uri.parse("https://discord.com/invite/$inviteCode")
-
-        // 尝试用 Discord App 打开
         val appIntent = Intent(Intent.ACTION_VIEW, discordUri).apply {
             `package` = "com.discord"
         }
         if (appIntent.resolveActivity(context.packageManager) != null) {
             context.startActivity(appIntent)
         } else {
-            // 回退到浏览器打开
             val browserIntent = Intent(Intent.ACTION_VIEW, webUri)
             context.startActivity(browserIntent)
         }
